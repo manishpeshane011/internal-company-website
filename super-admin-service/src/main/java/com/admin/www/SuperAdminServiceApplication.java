7e6ed477740a80e1bcad9ea327855e46a1b58e62
manish peshane <manishpeshane011@gmail.com>
2026-01-14 11:27:22 +0530

    Initial microservice architecture structure

---
+++ super-admin-service/src/main/java/com/admin/www/SuperAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.admin.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperAdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperAdminServiceApplication.class, args);
+	}
+
+}
---
+++ super-admin-service/src/test/java/com/admin/www/SuperAdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperAdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

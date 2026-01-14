cbef3ef246317b571972f71da180d7f5ebfdb820
manish peshane <manishpeshane011@gmail.com>
2026-01-14 15:18:12 +0530

    Describe what you changed

---
+++ different-role-se/src/test/java/com/role/www/DifferentRoleSeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.role.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DifferentRoleSeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
